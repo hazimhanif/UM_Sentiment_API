d4fbea375ef2b13458bc7cce55272243d784ef2d
Hazim Hanif <hazimhanif0@gmail.com>
2018-10-05 15:47:45 +0800

    Finalized backend test script

--- Scripts/backend_test.py
+++ Scripts/backend_test.py
@@ -5,7 +5,9 @@ Created on Fri Oct  5 10:29:52 2018
 @author: hazim
 
 This is the local backend code test bench for API deployment.
-
+Tested on:
+    -Tensorflow 1.10
+    -Keras 2.2.2
 """
 
 ## Import relevant libraries
@@ -14,10 +16,12 @@ import pickle
 import numpy as np
 from tensorflow.python.keras.preprocessing import sequence as keras_seq
 from tensorflow.python.keras.models import load_model
+import warnings
 
 ## Instance variables
+warnings.filterwarnings('ignore')
 INPUT_SIZE = 700
-text = 'I love this product very much. This is the best product ever!'
+text = 'The hotel was nice. However, the food is not good. The reception is great and very convincing.'
 
 
 def main():
@@ -27,20 +31,21 @@ def main():
         tokenizer = pickle.load(handle)
 
     ## Tokkenizing test data and create matrix
-    list_tokenized_test = tokenizer.texts_to_sequences(text)
+    list_tokenized_test = tokenizer.texts_to_sequences([text])
     x_test = keras_seq.pad_sequences(list_tokenized_test, 
                                      maxlen=INPUT_SIZE,
                                      padding='post')
     x_test = x_test.astype(np.int64)
-    
 
     ## Load the Keras-Tensorflow model
     model = load_model('../Models/mcp_erezeki_word_conv1D.hdf5')
     
-    ## Evaluate the model
-    print(model.predict_classes(x_test))
+    ## Predict using the loaded model
+    print('Text: ',text )
+    print('Predicted sentiment: ', 'Positive' if model.predict_classes(x_test)[0]==1 else 'Negative')
+    print('Probability of positive sentiment: ',model.predict_proba(x_test)[0][1])
+    print('Probability of negative sentiment:', model.predict_proba(x_test)[0][0])
 
-
-if __name__ == "main":
+if __name__ == '__main__':
     main()
     gc.collect()
